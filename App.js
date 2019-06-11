4b40aa90062ad388fe1b165cfad6840a7b83e7e7
Matheus Weber <matheus.weber@ibm.com>
2019-06-11 09:32:40 -0300

    Add mines rules

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import params from './src/params/'
-import Field from './src/components/Field'
+import MineField from './src/components/MineField'
+
+import {
+  createMinedBoard
+} from './src/functions'
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = this.createState()
+  }
+
+  minesAmount = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+
+    return Math.ceil(cols * rows * params.difficultyLevel)
+  }
+
+  createState = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return {
+      board: createMinedBoard(rows, cols, this.minesAmount)
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Startin Mines...</Text>
+        <Text style={styles.welcome}>Starting Mines...</Text>
         <Text style={styles.instructions}>Size: {params.getRowsAmount()} x {params.getColumnsAmount()}</Text>
-        <Field/>
-        <Field opened/>
-        <Field opened nearMines={1}/>
-        <Field opened mined exploded/>
-        <Field opened mined/>
-        <Field mined/>
-        <Field flagged/>
+        <View style={styles.board}>
+          <MineField board={this.state.board}></MineField>
+        </View>
       </View>
     );
   }
@@ -24,18 +45,11 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
+  },
+  board: {
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    backgroundColor: '#AAA'
+  }
+  
 });
--- src/components/MineField.js
+++ src/components/MineField.js
@@ -4,6 +4,16 @@ import Field from './Field'
 
 export default props => {
     const rows = props.board.map((row, r) => {
-        
+        const columns = row.map((field, c) => {
+            return <Field {...field} key={c}/>
+        })
+        return <View key={r} style={{flexDirection: 'row'}}>{columns}</View>
     })
+    return <View style={styles.container}>{rows}</View>
 }
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#EEE',
+    }
+})
