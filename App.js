c07fedbfc03c4090b16df9b8866c4ea46be78589
Matheus Weber <matheus.weber@ibm.com>
2019-06-11 09:55:56 -0300

    Add header

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {StyleSheet, Text, View, Alert} from 'react-native';
 import params from './src/params/'
 import MineField from './src/components/MineField'
+import Header from './src/components/Header'
 
 import {
   createMinedBoard,
@@ -10,7 +11,8 @@ import {
   hadExplosion,
   wonGame,
   showMines,
-  invertFlag
+  invertFlag,
+  flagsUsed
 } from './src/functions'
 import { tsMethodSignature } from '@babel/types';
 
@@ -71,8 +73,7 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Starting Mines...</Text>
-        <Text style={styles.instructions}>Size: {params.getRowsAmount()} x {params.getColumnsAmount()}</Text>
+        <Header flagsLeft={this.minesAmount() - flagsUsed(this.state.board)} onNewGame={() => this.setState(this.createState())}/>
         <View style={styles.board}>
           <MineField board={this.state.board} onOpenField={this.onOpenField} onSelectField={this.onSelectField}></MineField>
         </View>
